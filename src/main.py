10c4d220d2bfb8f976c4975320c7e0b5ece1bfc4
Irith Sharma <sharma.irith@gmail.com>
2024-05-04 11:24:45 -0400

    Update lambda return value

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from aws_lambda_powertools.logging import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
+import json
 from typing import (
     Any,
     Dict,
@@ -16,4 +17,12 @@ env_vars = load_env_vars()
 @logger.inject_lambda_context()
 def lambda_handler(event: Dict[str, Any], context: LambdaContext):
     logger.info("Lambda triggered", event=event, context=context)
-    return {}
+    return {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "body": json.dumps({}),
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "cookies": [],
+    }
