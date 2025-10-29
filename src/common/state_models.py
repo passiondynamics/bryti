a246227fb4c02fff8791f572e50a104f44eff7a3
Irith <71225312+irithsharma@users.noreply.github.com>
2025-10-29 19:52:24 +0000

    Split out counter-related functionality, add crime counter (#11)
    * Split out counter-related functionality, add crime counter
    
    * Enable crime commands
    
    * Add crime command tests

--- src/common/state_models.py
+++ src/common/state_models.py
@@ -54,12 +54,41 @@ class LookupFields(BaseModel):
     github_user_id: Optional[str] = None
 
 
-class DeathState(BaseModel):
+class CounterState(BaseModel):
     count: int
     last_timestamp: ISOUTCDatetime
+
+    def time_since(self, timestamp) -> str:
+        """
+        Create a relative time string based off the given timestamp.
+        """
+
+        time_since = timestamp - self.last_timestamp
+        s = time_since.seconds % 60
+        m = time_since.seconds // 60 % 60
+        h = time_since.seconds // 3600
+        d = time_since.days
+
+        # Consecutive conditions to prevent "0"s from being shown.
+        time_since_str = ""
+        if s > 0:
+            time_since_str = f"{s}s{time_since_str}"
+        if m > 0:
+            time_since_str = f"{m}m{time_since_str}"
+        if h > 0:
+            time_since_str = f"{h}h{time_since_str}"
+        if d > 0:
+            time_since_str = f"{d}d{time_since_str}"
+        if time_since_str:
+            time_since_str += " ago"
+        else:
+            time_since_str = "just now"
+
+        return time_since_str
 
 
 class State(LookupFields):
     members: Dict[str, Permission] = {}
-    deaths: Optional[DeathState] = None
+    deaths: Optional[CounterState] = None
+    crimes: Optional[CounterState] = None
     version: int = 0
